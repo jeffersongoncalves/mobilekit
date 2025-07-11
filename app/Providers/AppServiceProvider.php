74d35a49eaa368f39dbceb4c40df0d30eec74a5a
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-07-11 17:13:11 -0300

    Initial project

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use App\Providers\Filament\AdminPanelProvider;
+use App\Providers\Filament\AppPanelProvider;
+use App\Providers\Filament\GuestPanelProvider;
+use Filament\Support\Facades\FilamentView;
+use Filament\View\PanelsRenderHook;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        if (config('mobilekit.admin_panel_enabled', false)) {
+            $this->app->register(AdminPanelProvider::class);
+        }
+        if (config('mobilekit.app_panel_enabled', false)) {
+            $this->app->register(AppPanelProvider::class);
+        }
+        if (config('mobilekit.guest_panel_enabled', false)) {
+            $this->app->register(GuestPanelProvider::class);
+        }
+        FilamentView::registerRenderHook(PanelsRenderHook::HEAD_START, fn (): View => view('components.js-md5'));
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
